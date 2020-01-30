204787c1c0c1c402007acb3e1d434889b59d373a
feud72 <feud72@gmail.com>
2020-01-30 23:58:37 +0900

    Change Book model primary key -> ISBN

--- books/models.py
+++ books/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     publisher = models.CharField(max_length=100)
     pub_year = models.CharField(max_length=20, blank=True, null=True)
     volume = models.CharField(max_length=10, blank=True, null=True)
-    isbn = models.CharField(max_length=13, unique=True)
+    isbn = models.CharField(max_length=13, primary_key=True)
     kdc = models.CharField(max_length=10)
 
     def __str__(self):
