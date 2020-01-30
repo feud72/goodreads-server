78f50378449f27ac70c60ba0c1c29af2c8b39476
feud72 <feud72@gmail.com>
2020-01-30 12:56:11 +0900

    Add model books, seed books

---
+++ books/management/commands/seed_books.py
@@ -0,0 +1,48 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from books.models import Book
+
+
+class Command(BaseCommand):
+    help = "This command creates books from CSV"
+
+    def handle(self, *args, **options):
+
+        with open("data/BestLoanList_20200130_month.csv", encoding="euc-kr") as f:
+            f_csv = csv.reader(f)
+            next(f_csv)
+            for r in f_csv:
+                row = r
+                try:
+                    Book.objects.create(
+                        name=row[1],
+                        author=row[2],
+                        publisher=row[3],
+                        pub_year=row[4],
+                        volume=row[5],
+                        isbn=row[6],
+                        kdc=row[8],
+                    )
+                except Exception:
+                    pass
+
+        with open("data/BestLoanList_20200130_weekly.csv", encoding="euc-kr") as f:
+            f_csv = csv.reader(f)
+            next(f_csv)
+            for r in f_csv:
+                row = r
+                try:
+                    Book.objects.create(
+                        name=row[1],
+                        author=row[2],
+                        publisher=row[3],
+                        pub_year=row[4],
+                        volume=row[5],
+                        isbn=row[6],
+                        kdc=row[8],
+                    )
+                except Exception:
+                    pass
+        self.stdout.write(self.style.SUCCESS("Books created."))
---
+++ books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=500)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    pub_year = models.CharField(max_length=20, blank=True, null=True)
+    volume = models.CharField(max_length=10, blank=True, null=True)
+    isbn = models.CharField(max_length=13, unique=True)
+    kdc = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
