1d4d9a6a216c24297e98894150236e56e39eca89
feud72 <feud72@gmail.com>
2020-01-31 23:40:25 +0900

    기능 추가: term, year, author 파라미터로 검색 가능

--- books/views.py
+++ books/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +8,19 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "isbn"
+
+    def get_queryset(self):
+        term = self.request.query_params.get("term", None)
+        year = self.request.query_params.get("year", None)
+        author = self.request.query_params.get("author", None)
+        query_dic = {"name": term, "pub_year": year, "author": author}
+
+        filter_kwargs = {}
+        for k, v in query_dic.items():
+            if v is not None:
+                filter_kwargs["{}__icontains".format(k)] = v
+        try:
+            books = Book.objects.filter(**filter_kwargs)
+        except ValueError:
+            books = Book.objects.all()
+        return books
