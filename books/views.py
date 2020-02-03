36043a16fe414916e17b5c7b0added519576e6d4
feud72 <feud72@gmail.com>
2020-02-03 16:51:31 +0900

    Modify books/views.py

--- books/views.py
+++ books/views.py
@@ -10,22 +10,7 @@ class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
     lookup_field = "isbn"
 
-    def list(self, request, *args, **kwargs):
-        """
-        GET api/v1/books/
-        
-        책의 목록을 반환.
-
-        ---
-        ### 파라미터
-        - term    해당 단어가 포함된 책의 목록을 반환한다.
-
-        - year    해당 년도에 출판된 책의 목록을 반환한다.
-
-        - author  해당 저자가 포함된 책의 목록을 반환한다.
-
-        """
-
+    def get_queryset(self):
         term = self.request.query_params.get("term", None)
         year = self.request.query_params.get("year", None)
         author = self.request.query_params.get("author", None)
@@ -39,7 +24,23 @@ class BookViewSet(ModelViewSet):
             books = Book.objects.filter(**filter_kwargs)
         except ValueError:
             books = Book.objects.all()
-        queryset = books
+        return books
+
+    def list(self, request, *args, **kwargs):
+        """
+        GET api/v1/books/
+
+        책의 목록을 반환.
+
+        ---
+        ### 파라미터
+        - term    해당 단어가 포함된 책의 목록을 반환한다.
+
+        - year    해당 년도에 출판된 책의 목록을 반환한다.
+
+        - author  해당 저자가 포함된 책의 목록을 반환한다.
+
+        """
+        queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-
