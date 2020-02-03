439eb79ef3da519771189c31e5a85a0ca4d9a4a9
feud72 <feud72@gmail.com>
2020-02-03 16:39:34 +0900

    Signup, Login view

--- authentications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "authentications"
-
-urlpatterns = [
-    path("register", views.registrationView, name="register"),
-    path("login", views.loginView, name="login"),
-]
