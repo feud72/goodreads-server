56fa7f4b14656ec1720d286694e0ffa7b53caa07
feud72 <feud72@gmail.com>
2020-02-13 00:59:44 +0900

    문서화

--- config/yasg.py
+++ config/yasg.py
@@ -19,6 +19,8 @@ schema_view_v1 = get_schema_view(
 아래 기술은 API 명세에 대한 간략한 설명(인자, 쿼리 파라미터, 응답 등)을 제공합니다.
 실제 기능은 하단의 swagger의 Try it out을 작동시켜 확인할 수 있습니다. 
 
+---
+
 ## /accounts/
 계정 관련 회원가입, 로그인 기능을 담당합니다.
 인증 토큰을 요구하지 않습니다.
@@ -33,14 +35,16 @@ schema_view_v1 = get_schema_view(
 #### /accounts/login/ : 로그인
 필수 인자 : email, password
 응답
- - 성공시 200 {"message", "token", "id"}
- - 실패시 400 {"detail"} 또는 {"non_field_errors"} (JSON 형태로 반환)
+ - 성공 : 200 {"message", "token", "id"}
+ - 실패 : 400 {"detail"} 또는 {"non_field_errors"} (JSON 형태로 반환)
 
 #### /accounts/signup/ : 회원가입
 필수 인자 : email, password1, password2
 응답 
- - 성공시 201 {"message", "email"}
- - 실패시 400 {"detail"} 또는 {"non_field_errors"} (JSON 형태로 반환)
+ - 성공 : 201 {"message", "email"}
+ - 실패 : 400 {"detail"} 또는 {"non_field_errors"} (JSON 형태로 반환)
+
+---
 
 ## /books/
 책의 목록을 얻거나, 책의 ISBN을 입력하여 새로운 책을 만들 수 있습니다.
@@ -50,8 +54,8 @@ schema_view_v1 = get_schema_view(
 
 #### /books/ : DB가 가지고 있는 책의 목록을 반환합니다. 현재 페이지네이션은 25개로 고정.
 쿼리 파라미터
- - search : 검색어
- - page : 페이지
+ - search : 검색어 (옵션)
+ - page : 페이지 (옵션)
 
 #### /books/recommend/ : 도서관 인기 대출 순위에서 랜덤하게 10개를 추출하여 보여줍니다.
 쿼리 파라미터 : 없음
@@ -60,29 +64,34 @@ schema_view_v1 = get_schema_view(
 #### /books/search/ : 카카오 API를 통해 책을 검색하여 보여줍니다.
 (현재 버그로 search 쿼리 파라미터를 입력하지 않으면 server error 발생)  
 쿼리 파라미터
- - search : 검색어
- - page : 페이지
-응답 : 리스트를 반환합니다. 각각의 원소들은 GET /books/{isbn}/과 형식이 같습니다.
+ - search : 검색어 (필수)
+ - page : 페이지 (옵션)
+응답 
+ - 성공 : 200 리스트를 반환합니다. 각각의 원소들은 GET /books/{isbn}/과 형식이 같습니다.
 
 ### POST
 
 #### /books/ : 책을 생성합니다.
 필수 인자 : isbn
 응답
- - 성공시 201 {"title", "author", "publisher", "pub_year", "isbn", "description", "bookImage"} 
- - 실패시 400 {"isbn"} (JSON 형태로 반환)
+ - 성공 : 201 {"title", "author", "publisher", "pub_year", "isbn", "description", "bookImage"} 
+ - 실패 : 400 {"isbn"} (JSON 형태로 반환)
+
+(작성중)
+
+---
 
 ## /books/{isbn}/
 책의 상세 정보를 얻을 수 있습니다.
 인증 토큰을 요구하지 않습니다.
-
-
 
 ### GET
 
 ####/books/{isbn}/
 #### /books/{isbn}/recommend/ 
 #### /books/{isbn}/keyword/
+
+---
 
 ## /shelves/
 
@@ -95,6 +104,8 @@ schema_view_v1 = get_schema_view(
 ### POST
 
 #### /sheves/
+
+---
 
 ## /shelves/{id}/
 
@@ -109,6 +120,8 @@ schema_view_v1 = get_schema_view(
 ###PUT, PATCH, DELETE
 
 #### /shelves/{id}/
+
+---
 
 ## /shelves/{id}/memo/
 
