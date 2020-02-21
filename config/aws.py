75d3e1a2305d1d2827d21d388d89d75cf6de70a0
Ubuntu <ubuntu@ip-172-31-40-20.ap-northeast-2.compute.internal>
2020-02-21 07:56:44 +0000

    aws

--- config/aws.py
+++ config/aws.py
@@ -1,3 +1,6 @@
 DEBUG = False
-ALLOWED_HOSTS = ["ec2-54-180-97-10.ap-northeast-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = [
+    "ec2-54-180-97-10.ap-northeast-2.compute.amazonaws.com",
+    "54.180.97.10",
+]
 COOKIE_DOMAIN = "ec2-54-180-97-10.ap-northeast-2.compute.amazonaws.com"
